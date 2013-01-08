3eb999132a9a6a43e281f6d4b001c917b8789b38
Supy <justinc@sjsoft.com>
2013-01-08 14:49:55 +0200

    Re-scoped settings, improved exception handling and general code cleanup.

--- src/runner.py
+++ src/runner.py
@@ -1,13 +1,11 @@
-from settings import *
+import settings
 import time
 import os
 import email
 import string
-
-if EMAIL_USE_SSL:
-    from smtplib import SMTP_SSL as SMTP
-else:
-    from smtplib import SMTP
+import smtplib
+import sys
+from smtplib import SMTP
 
 from email import encoders
 from email.mime.base import MIMEBase
@@ -15,75 +13,108 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 def start():
+    if settings.EMAIL_TO == '':
+        exit_with_error('No receiver email address defined.')
+
+    if not os.path.isdir(settings.WHISPER_STORAGE_PATH):
+        exit_with_error('Invalid whisper storage path specified.')
+
     mail = MIMEMultipart()
-    mail['To'] = EMAIL_TO
-    mail['From'] = EMAIL_FROM
-    mail['Subject'] = EMAIL_SUBJECT_VALIDATION_KEY
+    mail['To'] = settings.EMAIL_TO
+    mail['From'] = settings.EMAIL_FROM
+    mail['Subject'] = settings.EMAIL_SUBJECT_VALIDATION_KEY
 
-    content = MIMEText(EMAIL_BODY_VALIDATION_KEY)
+    content = MIMEText(settings.EMAIL_BODY_VALIDATION_KEY)
     mail.attach(content)
-    files = walk_directory(WHISPER_STORAGE_PATH)
 
-    for path in files:
-        if not os.path.isfile(path):
-            continue
+    attached_files = 0
 
+    print "Scanning for files.."
+    for path in walk_directory(settings.WHISPER_STORAGE_PATH):
         attachment = setup_attachment(path)
-        mail.attach(attachment)
 
-    try:
-        conn = SMTP(EMAIL_HOST)
-        conn.set_debuglevel(False)
-        conn.login(EMAIL_USERNAME, EMAIL_PASSWORD)
+        if attachment:
+            mail.attach(attachment)
+            attached_files += 1
+
+    print "Found %d files for sending." % attached_files
+    if attached_files > 0:
         try:
-            conn.sendmail(mail['From'], mail['To'], mail.as_string())
-        finally:
-            conn.close()
+            print "Connecting.."
+            conn = SMTP(settings.EMAIL_HOST)
+            conn.set_debuglevel(False)
 
-    except Exception, exc:
-        print str(exc)
+            if settings.EMAIL_USE_TLS:
+                print "Starting TLS.."
+                conn.starttls()
+
+            print "Logging in.."
+            conn.login(settings.EMAIL_USERNAME, settings.EMAIL_PASSWORD)
+            print "Sending mail.."
+            conn.sendmail(mail['From'], mail['To'], mail.as_string())
+            print "Sent %d attachments." % attached_files
+        except smtplib.SMTPRecipientsRefused:
+            print "Receipient confused."
+        except smtplib.SMTPHeloError:
+            print "Server didn't respond properly to HELO."
+        except smtplib.SMTPSenderRefused:
+            print "Sender refused."
+        except smtplib.SMTPDataError:
+            print "Unexpected error code."
+        except Exception as exc:
+            print exc
+        finally:
+            if hasattr(conn, 'sock') and conn.sock:
+                conn.quit()
 
 def setup_attachment(path):
     ctype = 'application/octet-stream'
     maintype, subtype = ctype.split('/', 1)
 
     # Read in the contents of the attachment
-    fp = open(path, 'rb')
-    msg = MIMEBase(maintype, subtype)
-    msg.set_payload(fp.read())
-    fp.close()
-    encoders.encode_base64(msg)
-    msg.add_header('Content-Disposition', 'attachment', filename=path_to_metric_filename(path))
+    try:
+        fp = open(path, 'rb')
+        msg = MIMEBase(maintype, subtype)
+        msg.set_payload(fp.read())
+        encoders.encode_base64(msg)
+        msg.add_header('Content-Disposition', 'attachment', filename=path_to_metric_filename(path))
 
-    return msg
+        return msg
+    except IOError:
+        print "Error reading contents of %s" % path
+    finally:
+        fp.close()
+
+    return None
 
 def walk_directory(path):
-    file_list = []
     if isinstance(path, str):
         for root, folders, files in os.walk(path):
-            file_list.extend(select_files(root, files))
+            for f in select_files(root, files):
+                yield f
     elif isinstance(path, list) or isinstance(path, tuple):
-        paths = path
-        for path in paths:
-            for root, folders, files in os.walk(path):
-                file_list.extend(select_files(root, files))
+        for p in path:
+            if not os.path.isdir(p):
+                continue
 
-    return file_list
+            for root, folders, files in os.walk(p):
+                for f in select_files(root, files):
+                    yield f
 
 def select_files(dir, files):
-    file_list = []
     for filename in files:
         extension = os.path.splitext(filename)[1].strip()
         if extension == '.wsp':
-            file_list.append(os.path.join(dir, filename))
-    return file_list
-
+            yield os.path.join(dir, filename)
 
 def path_to_metric_filename(full_path):
-    relevant_path = full_path[len(WHISPER_STORAGE_PATH):].strip(os.sep)
+    relevant_path = full_path[len(settings.WHISPER_STORAGE_PATH):].strip(os.sep)
     metric_filename = relevant_path.replace(os.sep, '.')
     return metric_filename
 
+def exit_with_error(msg):
+    print msg
+    sys.exit()
 
 start()
 
--- src/settings.py
+++ src/settings.py
@@ -4,9 +4,9 @@ WHISPER_STORAGE_PATH = '/home/justinc/.graphite/storage/whisper/carbon/aggregato
 EMAIL_HOST = 'mail.sjsoft.com'
 EMAIL_USERNAME = 'justinc'
 EMAIL_PASSWORD = 'shuphJuem9'
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
 
-EMAIL_TO = 'justinc@sjsoft.com'
+EMAIL_TO = 'justin.cossutti@gmail.com'
 EMAIL_FROM = 'no-reply@sjsoft.com'
 EMAIL_SUBJECT_VALIDATION_KEY = 'j5_parsable'
 EMAIL_BODY_VALIDATION_KEY = 'CARBON_FILE_MAIL'
